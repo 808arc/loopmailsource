0845dc544f77db501f2422c92040c313a569404e
808arc <109691255+808arc@users.noreply.github.com>
2023-05-29 10:58:38 -0400

    made update of api_key inserting // add genius.timeout to avoid timeout error

--- main.py
+++ main.py
@@ -1,21 +1,28 @@
 import lyricsgenius
 import time
 
-#sleep = time.sleep(2)
-# add your Genius API access token here 
-genius = lyricsgenius.Genius('')
+# Open the file and read the API key
+with open('api_key.txt', 'r') as file:
+    api_key = file.read().strip()
 
-#genius = lyricsgenius.PublicAPI()
+sleep = time.sleep(2)
+# add your Genius API access token here 
+#genius = lyricsgenius.Genius('rtdnYUhhPhGP6VbV_aTmh18fqd_m1W1EiMNrSF-FuEI5vu9Zt5ArDmDncLaJnBKA')
+genius = lyricsgenius.Genius(api_key)
+
+
+# Increase the timeout duration to 10 seconds
+genius.timeout = 10
 
 # Search for 10 Drake songs using the 'search_artist' method
-artist = genius.search_artist("Drake", max_songs=4, include_features=True) 
-
+artist = genius.search_artist("Drake", max_songs=1)
+time.sleep(6) 
 
 # Iterate over the songs
 for song in artist.songs:
     # Retrieve the information of the song
-    song_info = genius.song(song.id(time.sleep(6))) 
-    
+    song_info = genius.song(song.id)
+
     # Iterate over the producer artists of the song
     for artist in song_info["song"]["producer_artists"]:
         print(artist["name"], sep=", ")
